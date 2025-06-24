80d6a8603b8f7f39ecb243040674bf9cc5d5b39d
Vladimir Poplavskij <float45@gmail.com>
2025-06-24 19:41:31 +0300

    Add dictionary

---
+++ dictionary/js/index.js
@@ -0,0 +1,611 @@
+const items = [
+  {
+    Lithuanian: "Aktyvinimas",
+    English: "activation",
+  },
+  {
+    Lithuanian: "Aktyvatorius",
+    English: "energizer",
+  },
+  {
+    Lithuanian: "Apdorojimas",
+    English: "processing,handling",
+  },
+  {
+    Lithuanian: "Atkūrimas",
+    English: "restore,recovery",
+  },
+  {
+    Lithuanian: "Atpažiniklis",
+    English: "identifier",
+  },
+  {
+    Lithuanian: "Atnaujinimas",
+    English: "updating",
+  },
+  {
+    Lithuanian: "Atranka",
+    English: "selection",
+  },
+  {
+    Lithuanian: "Atsakiklis",
+    English: "audio response unit",
+  },
+  {
+    Lithuanian: "Atšauktis",
+    English: "cancel",
+  },
+  {
+    Lithuanian: "Baigtis",
+    English: "finish,end,result",
+  },
+  {
+    Lithuanian: "Byla,rinkmena",
+    English: "file",
+  },
+  {
+    Lithuanian: "Blokuotė",
+    English: "blocking",
+  },
+  {
+    Lithuanian: "Braižytuvas",
+    English: "plotter",
+  },
+  {
+    Lithuanian: "Būgnas",
+    English: "drum",
+  },
+  {
+    Lithuanian: "Centravimas",
+    English: "centering",
+  },
+  {
+    Lithuanian: "Dalytuvas",
+    English: "divider",
+  },
+  {
+    Lithuanian: "Darbingumas",
+    English: "operability",
+  },
+  {
+    Lithuanian: "Darinys",
+    English: "construct",
+  },
+  {
+    Lithuanian: "Daugintuvas",
+    English: "multiplier",
+  },
+  {
+    Lithuanian: "Elektroninis laiko daviklis",
+    English: "electronic timer",
+  },
+  {
+    Lithuanian: "Dažnis",
+    English: "frequency, rate",
+  },
+  {
+    Lithuanian: "Delsa, vėlinimas,gaištis",
+    English: "delay, time delay",
+  },
+  {
+    Lithuanian: "Dešifratorius",
+    English: "decoder",
+  },
+  {
+    Lithuanian: "Diskasukis",
+    English: "disk driver",
+  },
+  {
+    Lithuanian: "Disponavimas",
+    English: "avaibility",
+  },
+  {
+    Lithuanian: "Duomenys",
+    English: "data, information",
+  },
+  {
+    Lithuanian: "Egzemplorius",
+    English: "copy",
+  },
+  {
+    Lithuanian: "Eiga",
+    English: "run, motion",
+  },
+  {
+    Lithuanian: "Eiliškumas",
+    English: "queque, order",
+  },
+  {
+    Lithuanian: "Eilutė",
+    English: "line, row, string",
+  },
+  {
+    Lithuanian: "Elgsena",
+    English: "behavior",
+  },
+  {
+    Lithuanian: "Ėmiklis",
+    English: "receptor",
+  },
+  {
+    Lithuanian: "Erdvė",
+    English: "space",
+  },
+  {
+    Lithuanian: "Etiketė",
+    English: "label",
+  },
+  {
+    Lithuanian: "Gaja",
+    English: "viablitiy",
+  },
+  {
+    Lithuanian: "Geba",
+    English: "ability,ableness,power",
+  },
+  {
+    Lithuanian: "Atminties gylis",
+    English: "memory derth",
+  },
+  {
+    Lithuanian: "Gaudyklė",
+    English: "trap, hook",
+  },
+  {
+    Lithuanian: "Gavėjas",
+    English: "acceptor",
+  },
+  {
+    Lithuanian: "Glaustumas",
+    English: "packing factor",
+  },
+  {
+    Lithuanian: "Grandinė",
+    English: "circuit",
+  },
+  {
+    Lithuanian: "Grandis",
+    English: "link",
+  },
+  {
+    Lithuanian: "Įdėtis",
+    English: "nesting",
+  },
+  {
+    Lithuanian: "Įeitis",
+    English: "input, entry",
+  },
+  {
+    Lithuanian: "Ieškiklis",
+    English: "searcher, finder",
+  },
+  {
+    Lithuanian: "Įgaliojimas",
+    English: "authorization",
+  },
+  {
+    Lithuanian: "Ilgamžiškumas",
+    English: "durability, lifetime",
+  },
+  {
+    Lithuanian: "Imitatorius",
+    English: "simulator",
+  },
+  {
+    Lithuanian: "Imtis",
+    English: "sample",
+  },
+  {
+    Lithuanian: "Imtuvas",
+    English: "receiver",
+  },
+  {
+    Lithuanian: "Įranga",
+    English: "equipment",
+  },
+  {
+    Lithuanian: "Įrašas",
+    English: "record",
+  },
+  {
+    Lithuanian: "Įrenginys",
+    English: "device",
+  },
+  {
+    Lithuanian: "Įsikišimas",
+    English: "intervention",
+  },
+  {
+    Lithuanian: "Įsiminimas",
+    English: "storing, saving",
+  },
+  {
+    Lithuanian: "Išdėstymas",
+    English: "placment,allocation",
+  },
+  {
+    Lithuanian: "Išimtis",
+    English: "exception",
+  },
+  {
+    Lithuanian: "Išjungiklis",
+    English: "on-off switch",
+  },
+  {
+    Lithuanian: "Iškeltis",
+    English: "dump",
+  },
+  {
+    Lithuanian: "Išlyginimas",
+    English: "aligment",
+  },
+  {
+    Lithuanian: "Išmetimas",
+    English: "ejection",
+  },
+  {
+    Lithuanian: "Išrinkimas",
+    English: "fetch",
+  },
+  {
+    Lithuanian: "Išsidėstymas",
+    English: "layout",
+  },
+  {
+    Lithuanian: "Išskaidymas",
+    English: "disassemling",
+  },
+  {
+    Lithuanian: "Išskyrimas",
+    English: "extraction, selection",
+  },
+  {
+    Lithuanian: "Įtaisas",
+    English: "unit, device",
+  },
+  {
+    Lithuanian: "Įtampa",
+    English: "voltage",
+  },
+  {
+    Lithuanian: "Jautrumas",
+    English: "sensivity, response",
+  },
+  {
+    Lithuanian: "Jungtis",
+    English: "connector",
+  },
+  {
+    Lithuanian: "Jutiklis",
+    English: "sensor",
+  },
+  {
+    Lithuanian: "Kaupiklis",
+    English: "accumulator",
+  },
+  {
+    Lithuanian: "Kelias",
+    English: "path, route",
+  },
+  {
+    Lithuanian: "Kopijuoklis",
+    English: "xerox",
+  },
+  {
+    Lithuanian: "Korpusas",
+    English: "case",
+  },
+  {
+    Lithuanian: "Kreivė",
+    English: "curve",
+  },
+  {
+    Lithuanian: "Laikmena",
+    English: "media",
+  },
+  {
+    Lithuanian: "Langas",
+    English: "window",
+  },
+  {
+    Lithuanian: "Ląstelė",
+    English: "cell",
+  },
+  {
+    Lithuanian: "Laukimas",
+    English: "wait",
+  },
+  {
+    Lithuanian: "Lygmuo",
+    English: "level",
+  },
+  {
+    Lithuanian: "Lizdas",
+    English: "socket",
+  },
+  {
+    Lithuanian: "Maketavimas",
+    English: "modelling",
+  },
+  {
+    Lithuanian: "Maketas",
+    English: "model",
+  },
+  {
+    Lithuanian: "Maitinimas",
+    English: "power supply",
+  },
+  {
+    Lithuanian: "Mažinimas",
+    English: "reduction",
+  },
+  {
+    Lithuanian: "Mėginys",
+    English: "sample , trial",
+  },
+  {
+    Lithuanian: "Naudingumas",
+    English: "efficiency",
+  },
+  {
+    Lithuanian: "Naudojimas",
+    English: "use,usage",
+  },
+  {
+    Lithuanian: "Padėtis",
+    English: "position",
+  },
+  {
+    Lithuanian: "Pagalba",
+    English: "help",
+  },
+  {
+    Lithuanian: "Paieška",
+    English: "searching",
+  },
+  {
+    Lithuanian: "Pakopa",
+    English: "stage",
+  },
+  {
+    Lithuanian: "Parama",
+    English: "support",
+  },
+  {
+    Lithuanian: "Paraštė",
+    English: "margin",
+  },
+  {
+    Lithuanian: "Parengtis",
+    English: "stand-by",
+  },
+  {
+    Lithuanian: "Pasirinktis",
+    English: "option",
+  },
+  {
+    Lithuanian: "Paskirsta",
+    English: "sharing",
+  },
+  {
+    Lithuanian: "Pataisa",
+    English: "correction",
+  },
+  {
+    Lithuanian: "Pateiktis",
+    English: "presentation",
+  },
+  {
+    Lithuanian: "Paviršius",
+    English: "surface",
+  },
+  {
+    Lithuanian: "Pereiga,šuolis",
+    English: "jump",
+  },
+  {
+    Lithuanian: "Perkrova",
+    English: "overload",
+  },
+  {
+    Lithuanian: "Pertrauktis",
+    English: "interrupt",
+  },
+  {
+    Lithuanian: "Peržvalga",
+    English: "browsing",
+  },
+  {
+    Lithuanian: "Plėstuvas",
+    English: "expander",
+  },
+  {
+    Lithuanian: "Pralaida",
+    English: "transmission",
+  },
+  {
+    Lithuanian: "Priedas",
+    English: "appendix",
+  },
+  {
+    Lithuanian: "Prieiga",
+    English: "access",
+  },
+  {
+    Lithuanian: "Prieinamumas",
+    English: "availability",
+  },
+  {
+    Lithuanian: "Prietaisas",
+    English: "device",
+  },
+  {
+    Lithuanian: "Prievadas",
+    English: "port",
+  },
+  {
+    Lithuanian: "Priežiūra",
+    English: "service",
+  },
+  {
+    Lithuanian: "Prijungimas",
+    English: "attaching",
+  },
+  {
+    Lithuanian: "Pristabdymas",
+    English: "suspending",
+  },
+  {
+    Lithuanian: "Raktas",
+    English: "key",
+  },
+  {
+    Lithuanian: "Redagavimas",
+    English: "editing",
+  },
+  {
+    Lithuanian: "Registruotuvas",
+    English: "logger",
+  },
+  {
+    Lithuanian: "Reikalavimai",
+    English: "requirements",
+  },
+  {
+    Lithuanian: "Reikmenys",
+    English: "accessories",
+  },
+  {
+    Lithuanian: "Rėmimas",
+    English: "support",
+  },
+  {
+    Lithuanian: "Režimas",
+    English: "mode,duty",
+  },
+  {
+    Lithuanian: "Ribotuvas",
+    English: "limiter",
+  },
+  {
+    Lithuanian: "Ryškumas",
+    English: "brightness,intensity",
+  },
+  {
+    Lithuanian: "Rūšiavimas",
+    English: "sorting",
+  },
+  {
+    Lithuanian: "Sąlyga",
+    English: "condition",
+  },
+  {
+    Lithuanian: "Seka",
+    English: "string,sequence",
+  },
+  {
+    Lithuanian: "Siuntėjas",
+    English: "sender",
+  },
+  {
+    Lithuanian: "Siųstuvas",
+    English: "transmitter",
+  },
+  {
+    Lithuanian: "Skyriklis",
+    English: "separator",
+  },
+  {
+    Lithuanian: "Skirstytuvas",
+    English: "distributor",
+  },
+  {
+    Lithuanian: "Skleistuvas",
+    English: "scanner",
+  },
+  {
+    Lithuanian: "Slinkitis",
+    English: "scrolling, rolling",
+  },
+  {
+    Lithuanian: "Spausdintuvas",
+    English: "printer",
+  },
+  {
+    Lithuanian: "Srautas",
+    English: "flow,stream",
+  },
+  {
+    Lithuanian: "Storis",
+    English: "width",
+  },
+  {
+    Lithuanian: "Suderinamumas",
+    English: "compatibility",
+  },
+  {
+    Lithuanian: "Sudėtuvas",
+    English: "summer",
+  },
+  {
+    Lithuanian: "Sujungimas",
+    English: "connection, coupling, join, link",
+  },
+  {
+    Lithuanian: "Sukeitimas",
+    English: "swapping",
+  },
+  {
+    Lithuanian: "Šerdis",
+    English: "core",
+  },
+  {
+    Lithuanian: "Šuolis,pereiga",
+    English: "jump",
+  },
+  {
+    Lithuanian: "Taisymas",
+    English: "repair",
+  },
+  {
+    Lithuanian: "Teiktis",
+    English: "report",
+  },
+  {
+    Lithuanian: "Tikrintuvas",
+    English: "verifier, checker",
+  },
+  {
+    Lithuanian: "Tinklelis",
+    English: "grid",
+  },
+];
+
+const searchInput = document.getElementById("searchInput");
+const list = document.getElementById("myList");
+
+function populateList() {
+  items.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${item.Lithuanian} | ${item.English}`;
+    list.appendChild(listItem);
+  });
+}
+
+populateList();
+
+function filterList() {
+  const filterValue = searchInput.value.toLowerCase();
+  const listItems = list.querySelectorAll("li");
+
+  listItems.forEach((item) => {
+    const text = item.textContent.toLowerCase();
+    if (text.includes(filterValue)) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+searchInput.addEventListener("input", filterList);
